e6d61dc4464854fdf0210992208970dea5aeeb25
Elie Steinbock <eliesteinbock@gmail.com>
2019-04-09 14:07:27 +0300

    updated to latest type graphql version

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,17 @@ import { setUpAccounts, userTypeDefs } from "./modules/user/accounts";
 import { setUpSegments } from "./modules/segment/setUp";
 
 // middleware
-import { MiddlewareFn } from 'type-graphql'
-import { Context } from 'apollo-server-core'
+import { MiddlewareFn } from "type-graphql";
+import { Context } from "apollo-server-core";
 
 // both these middlewares have issues:
 
 // based on: https://typegraphql.ml/docs/middlewares.html#simple-middleware
 export const LogAccess: MiddlewareFn<Context> = ({ context, info }, next) => {
-  console.log(context)
-  console.log(info)
-  return next()
-}
+  console.log(context);
+  console.log(info);
+  return next();
+};
 
 export const ResolveTime: MiddlewareFn = async ({ info }, next) => {
   const start = Date.now();
